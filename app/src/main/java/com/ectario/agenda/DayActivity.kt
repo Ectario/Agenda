38fa63398ed9a082aea58aec6a7d2ceee83676b6
Ectario <dimitritristangalaxy@gmail.com>
2021-05-29 21:35:31 +0200

    - Put the raw string and value in resource file - Add border for the hour column - Add coordinator to let the FAB corner right bottom

--- app/src/main/java/com/ectario/agenda/DayActivity.kt
+++ app/src/main/java/com/ectario/agenda/DayActivity.kt
@@ -1,15 +1,16 @@
 package com.ectario.agenda
 
-import android.annotation.SuppressLint
 import android.os.Bundle
+import android.view.Gravity
 import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import com.ectario.agenda.tools.*
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class DayActivity : DialogToAddSlots.AddSlotsDialogListener, AppCompatActivity() {
     private lateinit var day : Day
-    private lateinit var btn_add : FloatingActionButton
+    private lateinit var btnAdd : FloatingActionButton
 
     companion object {
         private var is_btn_add_clicked = false //to unable the spam click
@@ -18,14 +19,14 @@ class DayActivity : DialogToAddSlots.AddSlotsDialogListener, AppCompatActivity()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_day)
-        btn_add = findViewById(R.id.addSlot)
+        btnAdd = findViewById(R.id.addSlot)
         init()
     }
 
     private fun init(){
         day = DayHolder.currentDay
 
-        btn_add.setOnClickListener {
+        btnAdd.setOnClickListener {
             val scale = 1.4f
             if (!is_btn_add_clicked){
                 is_btn_add_clicked = true
@@ -44,16 +45,40 @@ class DayActivity : DialogToAddSlots.AddSlotsDialogListener, AppCompatActivity()
         refreshDisplay()
     }
 
-    @SuppressLint("SetTextI18n")
     private fun refreshDisplay(){
-        this.findViewById<LinearLayout>(R.id.linearLayoutDay).removeAllViews()
+        val activityColumnView = this.findViewById<LinearLayout>(R.id.activity_line_id)
+        val hourColumnView = this.findViewById<LinearLayout>(R.id.hour_column_id)
+
+        val listHour = ArrayList<Float>()
+
+        //Erase the past content
+        activityColumnView.removeAllViews()
+        hourColumnView.removeAllViews()
+
         day.timeSlots.sortBy { it.startTime }
         day.timeSlots.forEach {
-            val tv = TextView(applicationContext)
-            tv.text = HourSlot.formattingHour(it.startTime) + " to " + HourSlot.formattingHour(it.endTime) + " : " + it.slotName
-            tv.textSize = 20f
-            this.findViewById<LinearLayout>(R.id.linearLayoutDay)?.addView(tv)
+            listHour.add(it.startTime)
+            listHour.add(it.endTime)
+            val tvActivity = TextView(applicationContext)
+
+            tvActivity.text = it.slotName
+            tvActivity.textSize = 20f
+            tvActivity.gravity = Gravity.CENTER_HORIZONTAL
+
+            tvActivity.setTextColor(getColor(R.color.black))
+            activityColumnView.addView(tvActivity)
         }
+
+        listHour.sort()
+
+        listHour.forEach{
+            val tvHour = TextView(applicationContext)
+            tvHour.setMargins(top = 16)
+            tvHour.setTextColor(getColor(R.color.black))
+            tvHour.text = HourSlot.formattingHour(it)
+            hourColumnView.addView(tvHour)
+        }
+
     }
 
     private fun openDialog() {
--- app/src/main/java/com/ectario/agenda/tools/LayoutTools.kt
+++ app/src/main/java/com/ectario/agenda/tools/LayoutTools.kt
@@ -1,2 +1,44 @@
 package com.ectario.agenda.tools
 
+import android.content.Context
+import android.content.res.Resources
+import android.util.TypedValue
+import android.view.View
+import android.view.ViewGroup
+import android.widget.LinearLayout
+import androidx.core.view.marginBottom
+import androidx.core.view.marginLeft
+import androidx.core.view.marginRight
+import androidx.core.view.marginTop
+
+
+fun View.setMargins(
+    left: Int = this.marginLeft,
+    top: Int = this.marginTop,
+    right: Int = this.marginRight,
+    bottom: Int = this.marginBottom
+) {
+    var param = this.layoutParams as ViewGroup.MarginLayoutParams?
+    if (param == null) {
+        param =
+            LinearLayout.LayoutParams(
+                ViewGroup.LayoutParams.WRAP_CONTENT,
+                ViewGroup.LayoutParams.WRAP_CONTENT
+            )
+    }
+
+    param.setMargins(left, top, right, bottom)
+    this.layoutParams = param
+}
+
+fun Context.dpToPx(dp: Float): Int = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, dp, resources.displayMetrics).toInt()
+fun Context.spToPx(sp: Float): Int = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_SP, sp, resources.displayMetrics).toInt()
+
+
+fun getScreenWidth(): Int {
+    return Resources.getSystem().displayMetrics.widthPixels
+}
+
+fun getScreenHeight(): Int {
+    return Resources.getSystem().displayMetrics.heightPixels
+}
